21c4a5c1978fb6043682484590e99a124e97c09d
Kirill Kirillov <kirillovk@yahoo.co.nz>
2021-03-07 22:24:34 +1300

    improve styling, fix text

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -20,19 +20,19 @@ export class TimelineComponent implements OnInit {
         summary: 'Verizon Connect - Software Engineer',
         description: [
           'Worked as a full-stack software engineer on mobile and web products.',
-          'Utilized the following technologies Angular, React, Node.Js, C#, Kotlin, Java, SQL and Titanium (a cross-platform mobile framework)',
-          'Played a key role in the delivery of numerous products and features for the compliance side of the business',
-          'Presented a handful of talks on both technical and non-technical topics to the wider group I was working in and also a few to the whole office',
+          'Utilized the following technologies Angular, React, Node.Js, C#, Kotlin, Java, SQL and Titanium (a cross-platform mobile framework).',
+          'Played a key role in the delivery of numerous products and features for the compliance side of the business.',
+          'Presented a handful of talks on both technical and non-technical topics to the wider group I was working in and also a few to the whole office.',
         ],
       },
       {
         date: '2016',
         summary: 'University of Canterbury - Bachelor of Science in Computer Science',
         description: [
-          'Started university where I really developed my passion for software development and computer science',
+          'Started university where I really developed my passion for software development and computer science.',
           'Learnt how to study independently, work hard to achieve my goals and many other valuable lessons.',
           "One of the most memorable was that being a great software developer isn't always about coming up with the best solution to a problem, but its about how you deal with the consequences of the decisions made by your former colleagues or potentially even your past self.",
-          'Met some extraordinary people, made some life-long friends and memories',
+          'Met some extraordinary people, made some life-long friends and although it was fun it was also quite a challenge, but looking back now I think all the hard work definitely paid off.',
         ],
       },
     ];
